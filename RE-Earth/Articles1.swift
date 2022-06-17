4a0955889c92228429c38f77a06869c6a5bd3e7d
Eva Gonzalez <evagonzalez.w@gmail.com>
2022-06-17 12:03:14 -0400

    Fixed pages

---
+++ RE-Earth/Articles1.swift
@@ -0,0 +1,32 @@
+//
+//  Articles1.swift
+//  CameraUsePractice
+//
+//  Created by Eva Gonzalez Whitehouse on 6/17/22.
+//
+
+import UIKit
+
+class Articles1: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func ReadMore(_ sender: Any) {
+    } https://www.bbc.com/news/science-environment-61802802
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
